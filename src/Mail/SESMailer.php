1ee2c410e29a5cb9c03bfc9b01a5dda1a4dfbfca
Jordan Tuinman <jordan@mediasuite.co.nz>
2024-06-18 17:35:44 +1200

    WIP

--- src/Mail/SESMailer.php
+++ src/Mail/SESMailer.php
@@ -3,12 +3,14 @@
 namespace Symbiote\SilverStripeSESMailer\Mail;
 
 use Aws\Ses\SesClient;
-use SilverStripe\Control\Email\Mailer;
 use SilverStripe\Control\Email\Email;
 use SilverStripe\Core\Injector\Injector;
 use Exception;
 use Psr\Log\LoggerInterface;
-
+use Symfony\Component\Mailer\Bridge\Amazon\Transport\SesTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email as SymfonyEmail;
 
 /**
  * A mailer implementation which uses Amazon's Simple Email Service.
@@ -18,25 +20,19 @@ use Psr\Log\LoggerInterface;
  *
  * Does not support inline images.
  */
-class SESMailer implements Mailer
+class SESMailer implements MailerInterface
 {
-
 	/**
 	 * @var SesClient
 	 */
 	protected $client;
 
 	/**
-     * Uses QueuedJobs module when sending emails
-     *
-     * @var boolean
-     */
-	protected $useQueuedJobs = true;
-
-	/**
-	 * @var array|null
+	 * Uses QueuedJobs module when sending emails
+	 *
+	 * @var boolean
 	 */
-	protected $lastResponse = null;
+	protected $useQueuedJobs = true;
 
 	/**
 	 * @param array $config
@@ -59,17 +55,9 @@ class SESMailer implements Mailer
 	}
 
 	/**
-	 * @return array|null
+	 * @param SilverStripe\Control\Email $email
 	 */
-	public function getLastResponse()
-	{
-		return $this->lastResponse;
-	}
-
-	/**
-	 * @param SilverStripe\Control\Email
-	 */
-	public function send($email)
+	public function send($email): void
 	{
 		$destinations = $email->getTo();
 
@@ -93,79 +81,46 @@ class SESMailer implements Mailer
 			}
 		}
 
-		$destinations = array_keys($destinations);
 		$subject = $email->getSubject();
-		$rawMessageText = $email->render()->getSwiftMessage()->toString();
+		$message = $email->getBody();
 
 		if (class_exists(QueuedJobService::class) && $this->useQueuedJobs) {
 			$job = Injector::inst()->createWithArgs(SESQueuedMail::class, array(
 				$destinations,
 				$subject,
-				$rawMessageText
+				$message
 			));
 
 			singleton(QueuedJobService::class)->queueJob($job);
 
-			return true;
+			return;
 		}
 
 		try {
-			$response = $this->sendSESClient($destinations, $rawMessageText);
-
-			$this->lastResponse = $response;
+			$this->sendSESClient($destinations, $subject, $message);
 		} catch (\Aws\Ses\Exception\SesException $ex) {
 			Injector::inst()->get(LoggerInterface::class)->warning($ex->getMessage());
-
-			$this->lastResponse = false;
-			return false;
 		}
-
-        /* @var $response Aws\Result */
-        if (isset($response['MessageId']) && strlen($response['MessageId']) &&
-			(isset($response['@metadata']['statusCode']) && $response['@metadata']['statusCode'] == 200)) {
-            return true;
-        }
-
-		return false;
 	}
 
 	/**
 	 * Send an email via SES. Expects an array of valid emails and a raw email body that is valid.
 	 *
 	 * @param array $destinations array of emails addresses this email will be sent to
-	 * @param string $rawMessageText Raw email message text must contain headers; and otherwise be a valid email body
-	 * @return Array Amazon SDK response
+	 * @param string $subject Email subject
+	 * @param string $message Email message body
 	 */
-	public function sendSESClient ($destinations, $rawMessageText) {
+	protected function sendSESClient($destinations, $subject, $message)
+	{
+		$transport = new SesTransport($this->client);
+		$mailer = new Mailer($transport);
 
-		try {
-			$response = $this->client->sendRawEmail(array(
-				'Destinations' => $destinations,
-				'RawMessage' => array('Data' => $rawMessageText)
-			));
-		} catch (Exception $ex) {
-			/*
-			 * Amazon SES has intermittent issues with SSL connections being dropped before response is full received
-			 * and decoded we're catching it here and trying to send again, the exception doesn't have an error code or
-			 * similar to check on so we have to relie on magic strings in the error message. The error we're catching
-			 * here is normally:
-			 *
-			 * AWS HTTP error: cURL error 56: SSL read: error:00000000:lib(0):func(0):reason(0), errno 104
-			 * (see http://curl.haxx.se/libcurl/c/libcurl-errors.html) (server): 100 Continue
-			 *
-			 * Without the line break, so we check for the 'cURL error 56' as it seems likely to be consistent across
-			 * systems/sites
-			 */
-			if(strpos($ex->getMessage(), "cURL error 56")) {
-				$response = $this->client->sendRawEmail(array(
-					'Destinations' => $destinations,
-					'RawMessage' => array('Data' => $rawMessageText)
-				));
-			} else {
-				throw $ex;
-			}
-		}
+		$symfonyEmail = (new SymfonyEmail())
+			->from(Email::config()->get('admin_email'))
+			->to(...$destinations)
+			->subject($subject)
+			->text($message);
 
-		return $response;
+		$mailer->send($symfonyEmail);
 	}
 }
